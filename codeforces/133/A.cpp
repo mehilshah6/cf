14d5bbfafed1966d9c3a27ea8fdfd3a889207ff2
Mehil Shah <shahmehil6@gmail.com>
2022-05-13 09:01:00 +0530

    Add solution for problem `A - HQ9+` Link: https://codeforces.com/contest/133/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/133/submission/156969943

---
+++ codeforces/133/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#define len(v) ((int)((v).size()))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define chmax(x, v) x = max((x), (v))
+#define chmin(x, v) x = min((x), (v))
+using namespace std;
+using lli = long long int;
+
+void solve() {
+	string s;
+	cin >> s;
+	for (char c : s) {
+		if (c == 'H' || c == 'Q' || c == '9') {
+			cout << "YES";
+			return;
+		}
+	}
+	cout << "NO";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++)
+	solve();
+}
