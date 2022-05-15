f3c09bd184da5b60e7dc68a197a70ff3196d55ab
Mehil Shah <shahmehil6@gmail.com>
2022-05-15 11:47:00 +0530

    Add solution for problem `A - Bit++` Link: https://codeforces.com/contest/282/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/282/submission/157265643

---
+++ codeforces/282/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#define len(v) ((int)((v).size()))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define chmax(x, v) x = max((x), (v))
+#define chmin(x, v) x = min((x), (v))
+using namespace std;
+using lli = long long int;
+
+void solve() {
+	int n;
+	cin >> n;
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		string t;
+		cin >> t;
+		if (t.find("++") != std::string::npos) {
+			ans++;
+		} else {
+			ans--;
+		}
+	}
+	cout << ans;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++)
+	solve();
+}
