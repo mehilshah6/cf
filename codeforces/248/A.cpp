75125dba347610964cd785c3d23d714eb816cc59
Mehil Shah <shahmehil6@gmail.com>
2022-05-16 09:00:00 +0530

    Add solution for problem `A - Cupboards` Link: https://codeforces.com/contest/248/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/248/submission/157356234

---
+++ codeforces/248/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#define len(v) ((int)((v).size()))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define chmax(x, v) x = max((x), (v))
+#define chmin(x, v) x = min((x), (v))
+using namespace std;
+using lli = long long int;
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	vector<int> b(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i] >> b[i];
+	}
+	int element_left = count(a.begin(), a.end(), 0) > (n/2) ? 1 : 0;
+	int element_right = count(b.begin(), b.end(), 0) > (n/2) ? 1 : 0;
+	cout << count(a.begin(), a.end(), element_left) + count(b.begin(), b.end(), element_right);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++)
+	solve();
+}
