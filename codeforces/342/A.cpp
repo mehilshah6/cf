8caa33ba085c69bdad0ba8957a74c644cbb0f1a3
Mehil Shah <shahmehil6@gmail.com>
2022-05-16 09:55:00 +0530

    Add solution for problem `A - Xenia and Divisors` Link: https://codeforces.com/contest/342/problem/A Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/342/submission/157358270

--- codeforces/342/A.cpp
+++ codeforces/342/A.cpp
@@ -15,52 +15,61 @@ void solve() {
 	int n;
 	cin >> n;
 	int req = (n/3);
-	vector<int> arr(n);
+	vector<int> v(n);
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	if (count(arr.begin(), arr.end(), 1) < (n/3)) {
-		cout << "-1";
-		return;
-	}
-	vector<vector<int>> v (n/3, vector<int>(3, {-1}));
-	for (int i = 0; i < n/3; i++) {
-		v[i][0] = 1;
+		cin >> v[i];
 	}
 	vector<int> freq(8, 0);
 	for (int i = 0; i < n; i++) {
-		freq[arr[i]]++;
+		freq[v[i]]++;
 	}
-	int i = 0;
-	while (freq[2] > 0 && freq[4] > 0 && i < n/3) {
-		v[i][1] = 2;
-		v[i][2] = 4;
-		freq[2]--;
-		freq[4]--;
-		i++;
-	}
-	while (freq[2] > 0 && freq[6] > 0 && i < n/3) {
-		v[i][1] = 2;
-		v[i][2] = 6;
-		freq[2]--;
-		freq[6]--;
-		i++;
-	}
-	while (freq[3] > 0 && freq[6] > 0 && i < n/3) {
-		v[i][1] = 3;
-		v[i][2] = 6;
-		freq[3]--;
-		freq[6]--;
-		i++;
-	}
-	if (i < (n/3)) cout << "-1";
-	else {
-		for (auto i : v) {
-			for (auto j : i) {
-				cout << j << " ";
+	if (freq[1] >= req) {
+		if (freq[2] + freq[3] >= req) {
+			if (freq[4] + freq[6] >= req) {
+				int a = min(freq[2], freq[4]);
+				int count = 0;
+				int count124 = 0;
+				int count126 = 0;
+				int count136 = 0;
+				for (int j = 0; j < min(req, a); j++) {
+					count++;
+					count124++;
+				}
+				req -= a;
+				freq[2] -= a;
+				int b = min (freq[2], freq[6]);
+				for (int j = 0; j < min(req, b); j++) {
+					count++;
+					count126++;
+				}
+				req -= b;
+				freq[6] -= b;
+				int c = min (freq[3], freq[6]);
+				for (int j = 0; j < min(req, c); j++) {
+					count++;
+					count136++;
+				}
+				if (count < (n/3)) {
+					cout << "-1";
+				} else {
+					for (int i = 0; i < count124; i++) {
+						cout << "1 2 4" << endl;
+					}
+					for (int i = 0; i < count126; i++) {
+						cout << "1 2 6" << endl;
+					}
+					for (int i = 0; i < count136; i++) {
+						cout << "1 3 6" << endl;
+					}
+				}
+			} else {
+				cout << "-1";
 			}
-			cout << endl;
+		} else {
+			cout << "-1";
 		}
+	} else {
+		cout << "-1";
 	}
 }
 
