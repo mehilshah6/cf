3e4edde17ccef3248c1cd2b25eb98436dbaddfc6
Mehil Shah <shahmehil6@gmail.com>
2022-05-21 10:47:00 +0530

    Add solution for problem `A - Dubstep` Link: https://codeforces.com/contest/208/problem/A Tags: strings, *900 Ref: https://codeforces.com/contest/208/submission/157862500

---
+++ codeforces/208/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#define len(v) ((int)((v).size()))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define chmax(x, v) x = max((x), (v))
+#define chmin(x, v) x = min((x), (v))
+using namespace std;	
+using lli = long long int;
+std::string ltrim(const std::string &s)
+{
+    size_t start = s.find_first_not_of(" ");
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+ 
+std::string rtrim(const std::string &s)
+{
+    size_t end = s.find_last_not_of(" ");
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+ 
+std::string trim(const std::string &s) {
+    return rtrim(ltrim(s));
+}
+
+void solve() {
+	string s;
+	cin >> s;
+	while (s.find("WUB") != std::string::npos) {
+		s.replace(s.find("WUB"), 3, " ");
+	}
+	s = trim(s);
+	cout << s;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++)
+	solve();
+}
