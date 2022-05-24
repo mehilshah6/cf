de6e96d359bd97e65281df98a9d5ba840e78d506
Mehil Shah <shahmehil6@gmail.com>
2022-05-24 09:40:00 +0530

    Add solution for problem `B - Borze` Link: https://codeforces.com/contest/32/problem/B Tags: expression parsing, implementation, *800 Ref: https://codeforces.com/contest/32/submission/158261660

---
+++ codeforces/32/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#define len(v) ((int)((v).size()))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define chmax(x, v) x = max((x), (v))
+#define chmin(x, v) x = min((x), (v))
+using namespace std;	
+using lli = long long int;
+
+void solve() {
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.length(); ) {
+		if (s[i] == '.') {
+			cout << 0;
+			i++;
+		}
+		else if (s[i] == '-') {
+			if (s[i + 1] == '-') {
+				cout << 2;
+				i += 2;
+			} else {
+				cout << 1;
+				i += 2;
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++)
+	solve();
+}
