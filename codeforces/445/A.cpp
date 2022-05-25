5fa594925358f8de1e6b6bcc92bbe60b8dc04781
Mehil Shah <shahmehil6@gmail.com>
2022-05-25 09:21:00 +0530

    Add solution for problem `A - DZY Loves Chessboard` Link: https://codeforces.com/contest/445/problem/A Tags: dfs and similar, implementation, *1200 Ref: https://codeforces.com/contest/445/submission/158347624

---
+++ codeforces/445/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#define len(v) ((int)((v).size()))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define chmax(x, v) x = max((x), (v))
+#define chmin(x, v) x = min((x), (v))
+using namespace std;	
+using lli = long long int;
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	vector<vector<char>> v(n, vector<char>(m));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			char c;
+			cin >> c;
+			if (c == '-') {
+				cout << '-';
+			} else {
+				if ((i + j) % 2 == 0) {
+					cout << 'B';
+				} else {
+					cout << 'W';
+				}
+			}
+		}
+		cout << endl;
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++)
+	solve();
+}
