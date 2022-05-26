f6ad70ade67f2f7596a6f3f9f97d8d4192958178
Mehil Shah <shahmehil6@gmail.com>
2022-05-26 10:44:00 +0530

    Add solution for problem `A - The number of positions` Link: https://codeforces.com/contest/124/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/124/submission/158491514

---
+++ codeforces/124/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#define len(v) ((int)((v).size()))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define chmax(x, v) x = max((x), (v))
+#define chmin(x, v) x = min((x), (v))
+#define int long long int
+using namespace std;
+void solve() {
+	int n, a, b;
+	cin >> n >> a >> b;
+	cout << (n - max(a + 1, n - b) + 1);
+}
+int32_t main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++)
+	solve();
+}
